113b6bff6a9a72ed0cac4be46340f12c1c9eb63c
pranaynaik23 <pranaynaik007@hotmail.com>
2022-10-17 20:58:20 +0530

    main.py updated with new code

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+User_Input =input("Do you want to print(p) or exit without printing(q) :")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+while(User_Input):
+    if(User_Input=="p"):
+      print("Hello")
+      User_Input =input("Do you want to print(p) or exit without printing(q) :")
+    elif(User_Input=="q"):
+        User_Input= False
+    else:
+        break
